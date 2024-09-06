3afc82b6319ebc8081fbd1f240e50cf6847a9002
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-09-06 13:54:15 +0200

    try step-by-step chartjs

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,7 +1,35 @@
-<template></template>
+<template>
+    <div ref="barChart"></div>
+</template>
 
 <script setup>
-
+import { ref, onMounted } from 'vue'
 import Chart from 'chart.js/auto';
 
+const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+const barChart = ref()
+
+onMounted(() => {
+    const chart = new Chart(barChart.value, {
+        type: 'bar',
+        data: {
+            labels: data.map(row => row.year),
+            datasets: [
+            {
+                label: 'Acquisitions by year',
+                data: data.map(row => row.count)
+            }
+            ]
+        }
+    })
+})
 </script>
