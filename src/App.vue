21e9449e403f64ae5f03040bad05c69d08ab6ded
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-09-09 10:40:01 +0200

    made bar chart component

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+    <BarChart :data="data" label="test ok"></BarChart>
+</template>
+
+<script setup>
+import BarChart from './components/BarChart.vue';
+
+const data = [
+    { label: 2010, value: 10 },
+    { label: 2011, value: 20 },
+    { label: 2012, value: 15 },
+    { label: 2013, value: 25 },
+    { label: 2014, value: 22 },
+    { label: 2015, value: 30 },
+    { label: 2016, value: 28 },
+  ];
+</script>
--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,33 +1,34 @@
 <template>
-    <div ref="barChart"></div>
+    <canvas ref="barChart"></canvas>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import Chart from 'chart.js/auto';
 
-const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    },
+    options: Object,
+    label: String
+
+})
 
 const barChart = ref()
 
 onMounted(() => {
     const chart = new Chart(barChart.value, {
         type: 'bar',
+        options: props.options,
         data: {
-            labels: data.map(row => row.year),
+            labels: props.data.map(row => row.label),
             datasets: [
             {
-                label: 'Acquisitions by year',
-                data: data.map(row => row.count)
-            }
+                label: props.label,
+                data: props.data.map(row => row.value)
+            },
             ]
         }
     })
